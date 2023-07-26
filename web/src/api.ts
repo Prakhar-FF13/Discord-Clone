7a0640983e012bc10f9261b93e349738757c2746
Prakhar Kumar <prakhar.meerut@gmail.com>
2023-07-26 19:14:25 +0530

    Added Api call code

---
+++ web/src/api.ts
@@ -0,0 +1,36 @@
+import axios from "axios";
+import { User } from "./commonTypes";
+
+const apiClient = axios.create({
+  baseURL: "http://localhost:4000",
+  timeout: 1000,
+});
+
+export const login = async (data: User) => {
+  try {
+    const res = await apiClient.post("/login", data);
+    return {
+      error: false,
+    };
+  } catch (exception) {
+    return {
+      error: true,
+      message: "Failed to login, Something went wrong",
+    };
+  }
+};
+
+export const register = async (data: User) => {
+  try {
+    const res = await apiClient.post("/register", data);
+    console.log(res);
+    return {
+      error: false,
+    };
+  } catch (exception) {
+    return {
+      error: true,
+      message: "Failed to login, Something went wrong",
+    };
+  }
+};
