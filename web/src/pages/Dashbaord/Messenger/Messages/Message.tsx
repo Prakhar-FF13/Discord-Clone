56bdc46e1053cd1208fa3bb798c04b2b40925903
Prakhar Kumar <prakhar.meerut@gmail.com>
2023-08-08 18:38:45 +0530

    Dummy chat messages
    Signed-off-by: Prakhar Kumar <prakhar.meerut@gmail.com>

---
+++ web/src/pages/Dashbaord/Messenger/Messages/Message.tsx
@@ -0,0 +1,52 @@
+import { Typography, styled } from "@mui/material";
+import Avatar from "../../../../common/components/Avatar";
+
+const MainContainer = styled("div")({
+  width: "90%",
+  display: "flex",
+  marginTop: "10px",
+});
+
+const AvatarContainer = styled("div")({
+  width: "70px",
+});
+
+const MessageContainer = styled("div")({
+  display: "flex",
+  flexDirection: "column",
+});
+
+const MessageContent = styled("div")({
+  color: "#DCDDDE",
+});
+
+interface MessageProps {
+  email: string;
+  username: string;
+  content: string;
+  date: string;
+}
+
+const Message = ({ username, content, date }: MessageProps) => {
+  return (
+    <MainContainer>
+      <AvatarContainer>
+        <Avatar username={username} />
+      </AvatarContainer>
+      <MessageContainer>
+        <Typography
+          sx={{
+            fontSize: "16px",
+            color: "white",
+          }}
+        >
+          {username}{" "}
+          <span style={{ fontSize: "12px", color: "#72767d" }}>{date}</span>
+        </Typography>
+        <MessageContent>{content}</MessageContent>
+      </MessageContainer>
+    </MainContainer>
+  );
+};
+
+export default Message;
--- web/src/pages/Dashbaord/Messenger/Messages/index.tsx
+++ web/src/pages/Dashbaord/Messenger/Messages/index.tsx
@@ -2,6 +2,8 @@ import { ConnectedProps, connect } from "react-redux";
 import { RootState } from "../../../../store/store";
 import { styled } from "@mui/material";
 import MessagesHeader from "./MessagesHeader";
+import DUMMY_MESSAGES from "./DUMMY_MESSAGES";
+import Message from "./Message";
 
 const MainContainer = styled("div")({
   height: "calc(100% - 60px)",
@@ -15,6 +17,17 @@ const Messages = ({ chatDetails, messages }: ReduxChatState) => {
   return (
     <MainContainer>
       <MessagesHeader label={chatDetails ? chatDetails.label : ""} />
+      {DUMMY_MESSAGES.map((msg, idx) => {
+        return (
+          <Message
+            key={msg.id}
+            content={msg.content}
+            email={msg.email}
+            username={msg.username}
+            date={msg.date}
+          />
+        );
+      })}
     </MainContainer>
   );
 };
